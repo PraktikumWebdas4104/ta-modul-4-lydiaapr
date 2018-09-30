00e3ca5740db9c0d38409e9b057f3cc66f8dcb25
lydiaapr <43522408+lydiaapr@users.noreply.github.com>
2018-10-01 02:25:17 +0700

    Update formawal.php
    lydia 6701174016

--- formawal.php
+++ formawal.php
@@ -1,4 +1,47 @@
 <?php
+<html>
+<head>
+    <title> SILAHKAN LOGIN </title> 
+</head>
+    <tr>
+      <td>Username</td>
+      <td><input type="text" name="username"></td>
+    </tr>
+    <tr>
+       <td>Password</td>
+       <td><input type="password" name="password"></td>
+    </tr>
+    <tr>
+       <td></td>
+       <td><input type="submit" name="login" value="Log In"></td>
+       </tr>
+  </table>
+ </form>
+
+<?php 
+
+if(isset($_POST['login'])){
+  $username=$_POST['username'];
+  $user=$_POST['password'];
+
+  if($user=="admin"&&$pass="admin"){
+    echo "LOGIN BERHASIL";
+?>
+
+
+<a href="proses.php" > lanjut ke proses </a>
+<?php
+
+  }else{
+    echo "LOGIN GAGAL";
+  }
+
+}
+
+
+
+?>
+
 
 
 
